4cbcd78269e814bc643313bd0b5f7092472e6067
Tibi <szekeres.tibor97@gmail.com>
2022-11-14 19:09:14 +0100

    cd jump, first test in WIP

--- Assets/Scripts/Controllers/Player/PlayerMovement.cs
+++ Assets/Scripts/Controllers/Player/PlayerMovement.cs
@@ -6,19 +6,59 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
     
+    [SerializeField]
+    private float movementSpeed = 7f;
+    [SerializeField]
+    private float jumpForce = 7f;
+
+    private float jumpCooldown = 1.3f;
+    private float jumpTimer = 0.0f;
+
+    private bool canJump
+    {
+        get { return jumpTimer > jumpCooldown; }
+    }
+
     private void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();   
+        SetRigidBody(GetComponent<Rigidbody2D>());
     }
 
     private void Update()
     {
-        float dirX = Input.GetAxis("Horizontal");
-        rigidBody.velocity = new Vector2(dirX * 7f, rigidBody.velocity.y);
+        Move(Input.GetAxis("Horizontal"));
 
-        if (Input.GetButtonDown("Jump"))
+        Jump();
+    }
+
+    public void Move(float directionInput)
+    {
+        if (directionInput != 0)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7f, 0); 
+            var dirX = Input.GetAxis("Horizontal") > 0 ? 0.5f : -0.5f;
+            rigidBody.velocity = new Vector2(dirX * 7f, rigidBody.velocity.y);
         }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+    }
+
+    public void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && canJump)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, jumpForce, 0);
+            jumpTimer = 0.0f;
+        }
+        else if(!canJump)
+        {
+            jumpTimer += Time.deltaTime;
+        }
+    }
+
+    public void SetRigidBody(Rigidbody2D rigidbody)
+    {
+        this.rigidBody = rigidbody;
     }
 }
---
+++ Assets/Tests/PlayMode/MovementTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class MovementTest
+{
+
+    [UnityTest]
+    public IEnumerator Move()
+    {
+        var gameObject = new GameObject();
+        var rigidBody = gameObject.AddComponent<Rigidbody2D>();
+        var player = gameObject.AddComponent<PlayerMovement>();
+        player.SetRigidBody(rigidBody);
+        var startPosition = player.transform.position;
+
+        player.Move(0.1f);
+
+        yield return new WaitForSeconds(2f);
+        Debug.Log(player.transform.position.x + " | " + startPosition.x);
+        Assert.IsTrue(player.transform.position.x < startPosition.x);
+    }
+}
