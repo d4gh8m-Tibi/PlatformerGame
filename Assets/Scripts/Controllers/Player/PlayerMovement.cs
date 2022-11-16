f4465acb9d0c77ff1a35758c1743a81d4bb3b965
Rita Viczian <y2pbrt@inf.elte.hu>
2022-11-16 17:53:22 +0100

    Fix Player jump

--- Assets/Scripts/Controllers/Player/PlayerMovement.cs
+++ Assets/Scripts/Controllers/Player/PlayerMovement.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround;
     
     private void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();   
+        rigidBody = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -16,9 +20,14 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxis("Horizontal");
         rigidBody.velocity = new Vector2(dirX * 7f, rigidBody.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7f, 0); 
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
