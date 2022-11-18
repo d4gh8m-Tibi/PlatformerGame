a95ff28ead16a57c00ce8ac7493c95e568f66c00
BesenyeiGergelyRNE5K1 <rne5k1@INF.ELTE.HU>
2022-11-18 16:05:03 +0100

    Build Script added

---
+++ Build/BuildScript.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [Parameter()]
+    $unityPath="D:\Programs\2021.3.11f1\Editor\Unity.exe",
+    [Parameter()]
+    $proPath="D:\Projects\Toolsof\PlatformerGame"
+)
+echo "Starting Build"
+Start-Process -Wait -FilePath $unityPath -ArgumentList "-quit", "-batchmode", "-projectPath $proPath", "-executeMethod BuildScript.PerformBuild"
+echo "Finished Build"
