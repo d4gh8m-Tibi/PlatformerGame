23804ea4ef0f7e0a394a664244a87f7b8a9ee2ba
BesenyeiGergelyRNE5K1 <rne5k1@INF.ELTE.HU>
2022-11-18 19:45:38 +0100

    BuildScript debugged

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -1,14 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.VersionControl;
-using UnityEngine;
-
-public class BuildScript : MonoBehaviour
+public class BuildScript
 {
-  static void PerformBuild () {
-        string [] scene = { "Assets/Scenes/Menu/Menu.unity", "Assets/Scenes/Maps/Map_01.unity" };
-        BuildPipeline.BuildPlayer (scene, "./Build/Runnable/RUN.exe",
+    static void PerformBuild () {
+        string [] scene = { "./Assets/Scenes/Menu/Menu.unity", "./Assets/Scenes/Maps/Map_01.unity" };
+        BuildPipeline.BuildPlayer (scene,"./Builds/build.exe",
             BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 }
