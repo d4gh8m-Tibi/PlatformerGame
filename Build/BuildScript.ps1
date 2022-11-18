6fe4de2557398b3edf741f395517f28f8685d9c3
BesenyeiGergelyRNE5K1 <rne5k1@INF.ELTE.HU>
2022-11-18 16:50:03 +0100

    Buildscript and gitingore changes

--- Build/BuildScript.ps1
+++ Build/BuildScript.ps1
@@ -4,7 +4,6 @@
     [Parameter()]
     $proPath="D:\Projects\Toolsof\PlatformerGame"
 )
-echo "Neede software: Unity2021"
 echo "Starting Build"
 Start-Process -Wait -FilePath $unityPath -ArgumentList "-quit", "-batchmode", "-projectPath $proPath", "-executeMethod BuildScript.PerformBuild"
 echo "Finished Build"
